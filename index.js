b1fd269df34e30b4f9b9d8cce9f1539c525c35bd
Chbabula <126175655+Chbabula@users.noreply.github.com>
2023-08-01 09:10:10 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose  = require('mongoose')
+const cors = require('cors')
+const healthRoute=require('./routes/HealthRoute')
+const authRoute=require('./routes/AuthRoutes')
+
+
+
+
+
+const app= express();
+app.use(express.json());
+app.use(cors());
+
+//DATABASE CONNECTION
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.connection.once('connected', ()=>console.log("database connected"));
+mongoose.connection.on("error", (error)=>console.log("database error: ",error))
+
+
+//ROUTES
+
+app.use('/health', healthRoute)
+app.use('/api/v1/auth', authRoute)
+
+
+
+
+app.listen(process.env.SERVER_PORT,() => console.log(`App server started at ${process.env.SERVER_PORT}`));
