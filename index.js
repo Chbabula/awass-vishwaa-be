85054ec10a2c499e3c4668408f83442e07cd2813
Chbabula <126175655+Chbabula@users.noreply.github.com>
2023-08-09 22:07:14 +0530

    refactor: cors

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ const healthRoute=require('./routes/HealthRoute')
 const authRoute=require('./routes/AuthRoutes')
 
 
-// var whitelist = ['http://localhost:5173', /** other domains if any */ ]
-// var corsOptions = {
-//   credentials: true,
-//   origin: function(origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
 
 const app= express();
 app.use(express.json());
